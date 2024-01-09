561e7c8a681681717f61bdde78e58e893a1d22fd
gjmooney <gjmooney@asu.edu>
2024-01-09 11:44:24 +0100

    Refactor Popup and create DivOverlay to mirror leaflet changes

--- js/src/layers/Popup.ts
+++ js/src/layers/Popup.ts
@@ -2,30 +2,28 @@
 // Distributed under the terms of the Modified BSD License.
 
 import { WidgetView, unpack_models } from '@jupyter-widgets/base';
-import { MessageLoop } from '@lumino/messaging';
-import { Widget } from '@lumino/widgets';
 import { LatLngTuple, Popup, PopupOptions } from 'leaflet';
 import L from '../leaflet';
-import { LeafletUILayerModel, LeafletUILayerView } from './Layer';
-
-const DEFAULT_LOCATION = [0.0, 0.0];
+import { LeafletDivOverlayModel, LeafletDivOverlayView } from './DivOverlay';
+import { LeafletUILayerModel } from './Layer';
 
 type PopupContent = {
   msg: 'open' | 'close';
   location: LatLngTuple;
 };
 
-export class LeafletPopupModel extends LeafletUILayerModel {
+export class LeafletPopupModel extends LeafletDivOverlayModel {
   defaults() {
     return {
       ...super.defaults(),
       _view_name: 'LeafletPopupView',
       _model_name: 'LeafletPopupModel',
-      location: DEFAULT_LOCATION,
-      child: null,
-      min_width: 50,
-      max_width: 300,
-      max_height: null,
+      minWidth: 50,
+      maxWidth: 300,
+      maxHeight: undefined,
+      offset: [0, 7],
+      permanent: false,
+      direction: 'auto',
     };
   }
 }
@@ -35,10 +33,8 @@ LeafletPopupModel.serializers = {
   child: { deserialize: unpack_models },
 };
 
-export class LeafletPopupView extends LeafletUILayerView {
+export class LeafletPopupView extends LeafletDivOverlayView {
   obj: Popup;
-  child: LeafletPopupView; // Change the type as needed
-  child_promise: Promise<void>;
 
   create_obj() {
     this.obj = L.popup(this.get_options() as PopupOptions).setLatLng(
@@ -52,61 +48,18 @@ export class LeafletPopupView extends LeafletUILayerView {
     this.child_promise = Promise.resolve();
   }
 
-  render() {
-    super.render();
-    return this.set_child(this.model.get('child'));
-  }
-
-  remove() {
-    super.remove();
-    this.child_promise.then(() => {
-      if (this.child) {
-        this.child.remove();
-      }
-    });
-  }
-
-  set_child(value: LeafletPopupModel) {
-    if (this.child) {
-      this.child.remove();
-    }
-    if (value) {
-      this.child_promise = this.child_promise.then(async () => {
-        const view = await this.create_child_view<LeafletPopupView>(value);
-        MessageLoop.sendMessage(view.pWidget, Widget.Msg.BeforeAttach);
-        this.obj.setContent(view.el);
-        MessageLoop.sendMessage(view.pWidget, Widget.Msg.AfterAttach);
-        this.force_update();
-        this.child = view;
-        this.trigger('child:created');
-      });
-    }
-    return this.child_promise;
-  }
-
   leaflet_events() {
     super.leaflet_events();
-    this.obj.on('add', () => {
-      // This is a workaround for making maps rendered correctly in popups
-      window.dispatchEvent(new Event('resize'));
-    });
   }
 
   model_events() {
     super.model_events();
-    this.model.on('change:child', () => {
-      this.set_child(this.model.get('child'));
-    });
+
     this.model.on_some_change(
       ['min_width', 'max_width', 'max_height'],
-      this.update_popup,
+      this.update,
       this
     );
-  }
-
-  update_popup() {
-    L.setOptions(this.obj, this.get_options());
-    this.force_update();
   }
 
   force_update() {
@@ -119,6 +72,7 @@ export class LeafletPopupView extends LeafletUILayerView {
       this.map_view.obj.closePopup(this.obj);
     }
   }
+
   handle_message(content: PopupContent) {
     //const objContent = this.obj.getContent();
 
